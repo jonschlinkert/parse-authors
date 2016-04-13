4f6ea693028477a98de6fbe651a8bcda48ba5884
Alexander Wunschik <alex@wunschik.net>
2016-04-13 14:00:33 +0200

    satisfied some tests

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var author = require('parse-author');
 module.exports = function(authors) {
   if (typeof authors === 'string') {
     authors = authors.split(/[\r\n]+/).filter(Boolean);
+    authors = authors.length ? authors : [''];
   }
   return authors.reduce(function(acc, str) {
     return acc.concat(author(str));
